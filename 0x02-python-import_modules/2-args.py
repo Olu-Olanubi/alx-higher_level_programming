a6814f87935f39373b6f1b77575e7aa500265139
Olu-Olanubi <seyeolanubi@gmail.com>
2023-01-05 19:49:30 -0500

    program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    size = len(argv) - 1
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) != 1 else "argument",
+                 "." if (size) == 0 else ":"))
+    for i in range(size):
+        print("{:d}: {:s}".format(i + 1, argv[i+1]))
