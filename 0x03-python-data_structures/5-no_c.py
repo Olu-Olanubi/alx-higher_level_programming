a2774baf22e8f59d0e27d608c8d75eed4c150723
Olu-Olanubi <seyeolanubi@gmail.com>
2023-01-09 18:32:31 -0500

    Script that removes the letter c or C from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
-    for c in my_string:
-        if c not in 'Cc':
-            new_string += c
-    return new_string
+    return ("".join(c for c in my_string if c not in "Cc"))
