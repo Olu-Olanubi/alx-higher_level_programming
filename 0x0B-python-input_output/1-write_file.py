4e8981afa216c77adcf37465081a6cb769811475
Olu-Olanubi <seyeolanubi@gmail.com>
2023-02-07 17:05:19 -0500

    function that writes a string to a text file (UTF8) and returns the number of characters written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
+"""
+Module 3-write_file
+Contains function that writes to text file and returns num chars written
+"""
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8)
-       and returns the number of characters written
-    Args:
-        filename (str): string of path to file
-        text (str): text to be written to file
-    Returns:
-        number of lines in file
-    """
+    """writes to text file and returns num chars written"""
     with open(filename, mode="w", encoding="utf-8") as f:
-        f.write(text)
-
-    with open(filename, mode="r", encoding="utf-8") as f:
-        return(len(f.read()))
+        return (f.write(text))
