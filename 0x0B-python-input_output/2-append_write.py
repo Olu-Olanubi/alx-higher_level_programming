753f33f2412eb8db48d1cdea26cac4d5e1bfb605
Olu-Olanubi <seyeolanubi@gmail.com>
2023-02-07 23:48:00 -0500

    append_write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+"""
+Module 4-append_write
+Contains function that appends to text file and returns num chars added
+"""
 
 
 def append_write(filename="", text=""):
-    """Write string to file (append mode)
-    Args:
-        filename (str): string of path to file
-        text (str): string to write to file
-    Returns:
-        number of characters written
-    """
+    """appends to text file and returns num chars added"""
     with open(filename, mode="a", encoding="utf-8") as f:
-        return (f.write(text))
+        return(f.write(text))
+
