921b72f6018c4f401c7ef3bb58b3c82d6ff6cf48
Olu-Olanubi <seyeolanubi@gmail.com>
2023-02-16 22:06:02 -0500

    Installed pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,36 @@
 #!/usr/bin/python3
+"""
+Module contains class Base
+Contains private class __nb_objects, and class constructor __init__
+Returns JSON string representation of list dictionaries
+Saves JSON strings of instance dictionaries into file
+Returns Python obj of JSON string representation
+Returns instance with attributes already set
+Returns list of instances
+Saves to CSV and loads from CSV file
+"""
+
 
 import json
 import csv
 
-class Base:
+
+class Base():
+    """
+    defines class Base
+    Class Attributes:
+        __nb_objects
+    Methods:
+        __init__(self, id=None)
+    Static Methods:
+        to_json_string(list_dictionaries)   from_json_string(json_string)
+    Class Methods:
+        save_to_file(cls, list_objs)        save_to_file_csv(cls, list_objs)
+        load_from_file(cls)                 load_from_file_csv(cls)
+        create(cls, **dictionary)
+    """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Initialize id, increment class attribute if no id and set as id"""
         if id:
@@ -52,15 +78,15 @@ class Base:
     def load_from_file(cls):
         """Returns list of instances"""
         filenamee = cls.__name__ + ".json"
-        l = []
+        line = []
         try:
             with open(filenamee, "r") as f:
                 instances = cls.from_json_string(f.read())
             for i, dic in enumerate(instances):
-                l.append(cls.create(**instances[i]))
-        except:
+                line.append(cls.create(**instances[i]))
+        except FileNotFoundError:
             pass
-        return l
+        return line
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
