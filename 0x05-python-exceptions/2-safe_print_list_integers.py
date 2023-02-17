d615faf74a10deb07f4a330fdec73e148c107e07
Olu-Olanubi <seyeolanubi@gmail.com>
2023-02-17 10:50:30 -0500

    Prints only integers in a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+            print("Index out of range")
+        else:
+            count = count + 1
+    print("")
+    return(count)
